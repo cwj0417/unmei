439d23ba5392a18eb2e17ac768f5b3b70cb9d711
test <chenwenjun@yo-cwj.com>
2023-12-18 15:06:33 +0800

    feat: ui

--- src/components/components/EarthlyBranchIcon.vue
+++ src/components/components/EarthlyBranchIcon.vue
@@ -1,12 +1,12 @@
 <template>
-<span :style="getRenderStyle(src?.藏干[0].五行?.type,src?.藏干[0].阴阳)">{{src?.type}}</span>
+<span :style="getRenderStyle(src?.藏干[0].五行?.type, src?.藏干[0].阴阳, size)">{{src?.name}}</span>
 <span v-if="showHs" v-for="(item, index) of src?.藏干" :style="{'font-size': (20 - index * 4) + 'px'}">
-    <HeavenlyStemIcon :src="item" />
+    <HeavenlyStemIcon :size="size" :src="item" />
 </span>
 </template>
 <script setup>
 import HeavenlyStemIcon from './HeavenlyStemIcon.vue'
 import { getRenderStyle } from '../../service/util';
 
-defineProps(['src', 'showHs'])
+defineProps(['src', 'showHs', 'size'])
 </script>
--- src/components/components/HeavenlyStemIcon.vue
+++ src/components/components/HeavenlyStemIcon.vue
@@ -1,8 +1,8 @@
 <template>
-<span :style="getRenderStyle(src?.五行?.type,src?.阴阳)">{{ src?.name }}</span>
+<span :style="getRenderStyle(src?.五行?.type, src?.阴阳, size)">{{ src?.name }}</span>
 </template>
 <script setup>
 import { getRenderStyle } from '../../service/util';
 
-defineProps(['src'])
+defineProps(['src', 'size'])
 </script>
