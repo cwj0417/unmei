f099129cf6f53ef24fa4526bb961e07d61df507b
cwj0417 <chenwenjun@yo-cwj.com>
2024-02-16 01:12:48 +0800

    chore: roll back build configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    target: 'es2015'
-  }
+  plugins: [vue()]
 })
